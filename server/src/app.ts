e6225e9acbb5b740fe01c40df18330721159ad55
Minh Le <qminh279@gmail.com>
2024-01-05 23:29:52 -0500

    refactor app from index

---
+++ server/src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import config from "./utils/config";
+import helmet from "helmet";
+import morgan from "morgan";
+import 'express-async-errors';
+
+const app = express();
+
+// MONGODB connection
+mongoose.set("strictQuery", false);
+console.log("connecting to", config.MONGODB_URI);
+mongoose
+  .connect(config.MONGODB_URI as string)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error: unknown) => {
+    let errorMessage = "Something went wrong: ";
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    console.error("error connecting to MongoDB:", errorMessage);
+  });
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(morgan("common"));
+
+
+export default app;
